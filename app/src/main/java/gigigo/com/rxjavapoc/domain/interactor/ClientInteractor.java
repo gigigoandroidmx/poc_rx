2d0c2bad6867efb9ceff1e6d93f698b018345bca
Carlos Garcia <carlos.garcia@gigigo.com.mx>
2017-12-15 12:34:49 -0600

    Prueba con retry ok

--- app/src/main/java/gigigo/com/rxjavapoc/domain/interactor/ClientInteractor.java
+++ app/src/main/java/gigigo/com/rxjavapoc/domain/interactor/ClientInteractor.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import gigigo.com.rxjavapoc.domain.entity.ClientResponse;
 import gigigo.com.rxjavapoc.network.IpirangaService;
 import io.reactivex.Observable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -30,20 +31,53 @@ public class ClientInteractor extends KInteractor {
         return new ServiceClient().createService(IpirangaService.class)
                 .client(numClient, clientId, clientSecret, userCredentials)
                 //.retry()
-                /*.retryWhen(attempts -> {
-                    return attempts.switchMap(o -> {
-                        if (++retryCount < maxRetries) {
-                            // When this Observable calls onNext, the original
-                            // Observable will be retried (i.e. re-subscribed).
-                            return Observable.timer(4, TimeUnit.SECONDS);
-                        }
-                        // Max retries hit. Just pass the error along.
-                        return Observable.error(new Throwable("Error de red", o.getCause()));
-                    });
-                })*/
+                .retryWhen(new RetryWithDelay(3, 4000))
                 .subscribeOn(Schedulers.io());
     }
 
     @Override
     public void run() {}
+
+    public class RetryWithDelay implements Function<Observable<? extends Throwable>, Observable<?>> {
+        private final int maxRetries;
+        private final int retryDelayMillis;
+        private int retryCount;
+
+        public RetryWithDelay(final int maxRetries, final int retryDelayMillis) {
+            this.maxRetries = maxRetries;
+            this.retryDelayMillis = retryDelayMillis;
+            this.retryCount = 0;
+        }
+
+        @Override
+        public Observable<?> apply(final Observable<? extends Throwable> attempts) {
+            return attempts
+                    .flatMap(new Function<Throwable, Observable<?>>() {
+                        @Override
+                        public Observable<?> apply(final Throwable throwable) {
+                            if (++retryCount < maxRetries) {
+                                // When this Observable calls onNext, the original
+                                // Observable will be retried (i.e. re-subscribed).
+                                return Observable.timer(retryDelayMillis,
+                                        TimeUnit.MILLISECONDS);
+                            }
+
+                            // Max retries hit. Just pass the error along.
+                            return Observable.error(throwable);
+                        }
+                    });
+        }
+
+        /*.retryWhen(attempts -> {
+            return attempts.switchMap(o -> {
+                if (++retryCount < maxRetries) {
+                    // When this Observable calls onNext, the original
+                    // Observable will be retried (i.e. re-subscribed).
+                    return Observable.timer(4, TimeUnit.SECONDS);
+                }
+                // Max retries hit. Just pass the error along.
+                return Observable.error(o.fillInStackTrace());
+            });
+        })*/
+    }
 }
